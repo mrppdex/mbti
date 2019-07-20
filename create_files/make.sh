a546196797ec2537e8362417edda180230c4e3cf
Pawel Piela <aspiela@gmail.com>
2019-07-20 18:40:05 +0100

    updated script

--- create_files/make.sh
+++ create_files/make.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-if ([ -z $1 ] || [ -z $2 ])
+if ( [ -n $1 ] || [ -d $2] )
 then
-    echo "args are missing"
-    echo "Usage: ./make.sh [PORT] [DICHOTOMY]"
-    echo "e.g. ./make.sh 30001 EI"
+    python mtbi.etl.dense.py
+    python mtbi.feature_eng.dense.py $1
+    python mtbi.model_def.dense.py
+    python mtbi.model_train.dense.py
+
+    cp *.pkl ../model_container/data/
+    cp trained_model.h5 ../model_container/data
     exit 0
 fi
 
-python mtbi.etl.dense.py
-python mtbi.feature_eng.dense.py $2
-python mtbi.model_def.dense.py
-python mtbi.model_train.dense.py
-
-cp *.pkl ../model_container/data/
-cp trained_model.h5 ../model_container/data
-
-export PORT=$1
-export DICHOTOMY=$2
-
-docker_tag=$(echo "mrppdex/mbti_$DICHOTOMY" | awk '{print tolower($0)}')
-
-docker build --tag=$docker_tag ../model_container/.
-docker push $docker_tag
+echo "args are missing"
+echo "Usage: ./make.sh [ EI | SN | FT | PJ ]"
+echo "e.g. ./make.sh EI"
