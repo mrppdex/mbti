11a8719fe3b4cfb8ba7bb5318b1badf9a77304ba
Pawel Piela <aspiela@gmail.com>
2019-07-20 22:25:08 +0100

    updated docker image names

--- create_files/make.sh
+++ create_files/make.sh
@@ -3,6 +3,10 @@
 echo "Usage: ./make.sh [ EI | SN | FT | PJ ] <model data dir>"
 echo "[] - REQUIRED, <> - OPTIONAL"
 
+if [[ -z ${1} ]]; then
+  exit 1
+fi
+
 DATA_DIR=${2}
 DATA_DIR=${DATA_DIR:='../model_container/data/'}
 if [[ -d ${DATA_DIR} ]]
