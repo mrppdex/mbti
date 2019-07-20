22e14b63d28c4ef18cb9c8f610609d1cd4bb79a9
Pawel Piela <aspiela@gmail.com>
2019-07-20 19:31:46 +0100

    improved model files make.sh

--- create_files/make.sh
+++ create_files/make.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-if ( [ -n $1 ] || [ -d $2] )
+
+DATA_DIR=${2}
+DATA_DIR=${DATA_DIR:='../model_container/data/'}
+if [[ -d ${DATA_DIR} ]]
 then
     python mtbi.etl.dense.py
     python mtbi.feature_eng.dense.py $1
     python mtbi.model_def.dense.py
     python mtbi.model_train.dense.py
 
-    cp *.pkl ../model_container/data/
-    cp trained_model.h5 ../model_container/data
+    cp *.pkl ${DATA_DIR}
+    cp trained_model.h5 ${DATA_DIR}
     exit 0
 fi
 
 echo "args are missing"
-echo "Usage: ./make.sh [ EI | SN | FT | PJ ] [model data dir]"
-echo "e.g. ./make.sh EI /mnt/c/mbti_docker/model_container/data"
+echo "Usage: ./make.sh [ EI | SN | FT | PJ ] <model data dir>"
+echo "[] - REQUIRED, <> - OPTIONAL"
